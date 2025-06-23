8f156b84fd2da48f340556e1d6f807a5ef55c62d
Pedro Gonçalves <1986200+mapherez@users.noreply.github.com>
2025-06-23 23:12:12 +0100

    Add prop diffing and throttled render events

--- README.md
+++ README.md
@@ -11,7 +11,14 @@ npm install lit-profiler-helper
 ```ts
 import { enableLitProfiler } from 'lit-profiler-helper';
 
-enableLitProfiler({ logToConsole: true, emitEvents: true });
+enableLitProfiler({
+  logToConsole: true,
+  emitEvents: true,
+  trackProperties: true,
+});
 ```
+
+Setting `trackProperties` to `true` will include a deep diff of component
+properties in render events for easier inspection.
 
 Optionally pair this library with a Chrome DevTools extension for better visualization.
--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,11 +1,42 @@
-export function throttle<T extends (...args: any[]) => void>(fn: T, wait: number): T {
+export interface ThrottleOptions {
+  leading?: boolean;
+  trailing?: boolean;
+}
+
+/**
+ * Throttle calls to `fn` so that it executes at most once every `wait` ms.
+ * By default it triggers on the leading edge only but can optionally trigger on
+ * the trailing edge of the burst as well.
+ */
+export function throttle<T extends (...args: any[]) => any>(
+  fn: T,
+  wait: number,
+  options: ThrottleOptions = {}
+): (...args: Parameters<T>) => void {
+  const { leading = true, trailing = false } = options;
   let timer: number | undefined;
-  return function(this: unknown, ...args: Parameters<T>) {
-    if (timer === undefined) {
+  let lastArgs: Parameters<T> | undefined;
+  let lastThis: any;
+
+  return function (this: any, ...args: Parameters<T>) {
+    if (!timer) {
+      if (leading) {
+        fn.apply(this, args);
+      } else if (trailing) {
+        lastArgs = args;
+        lastThis = this;
+      }
       timer = window.setTimeout(() => {
         timer = undefined;
+        if (trailing && lastArgs) {
+          fn.apply(lastThis, lastArgs);
+          lastArgs = undefined;
+          lastThis = undefined;
+        }
       }, wait);
-      fn.apply(this, args);
+    } else if (trailing) {
+      lastArgs = args;
+      lastThis = this;
     }
-  } as T;
+  };
 }
